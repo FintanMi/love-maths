fffa3e2c58ee21929a87fa155fac29463f9ed108
FintanMi <fintan.mitchell5@mail.dcu.ie>
2023-03-06 22:18:53 +0000

    Add division code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -43,6 +43,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractionQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivideQuestion(num1, num2);
     } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
@@ -86,6 +88,8 @@ function calculateCorrectAnswer() {
         return[operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return[operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return[operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -132,6 +136,11 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 }
 
-function displayDivideQuestion() {
+function displayDivideQuestion(operand1, operand2) {
+
+    operand1 = operand1*operand2
+        document.getElementById('operand1').textContent = operand1;
+        document.getElementById('operand2').textContent = operand2;;
+        document.getElementById('operator').textContent = "/";
 
 }
